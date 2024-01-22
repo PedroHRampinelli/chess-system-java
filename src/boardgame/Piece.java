04870b4e2d28b332f9af99fe0a0930a3c3f9d77f
PedroHRampinelli <pedrorampinelli41@hotmail.com>
2024-01-22 17:50:15 -0300

    cria classes board e piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,14 @@
+package boardgame;
+
+public class Piece {
+
+    protected Position position;
+    private Board board;
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+    protected Board getBoard() { //Somente classes dentro do mesmo pacote e subclasses podem acessar
+        return board;
+    }
+}
